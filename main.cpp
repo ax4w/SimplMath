f107d4121557a64ac70a73aed059e3f3f1b3876c
0x3alex <a-wrede@outlook.de>
2022-10-23 18:52:36 +0200

    reduced usage of new

--- main.cpp
+++ main.cpp
@@ -6,18 +6,29 @@
 using namespace sclui;
 
 static std::vector<int> results = {};
+static std::vector<BasicItem *> addresses = {};
 
-TextBox *minimum = nullptr;
-TextBox *maximum = nullptr;
-Screen *game = nullptr;
+TextBox *minimum = new TextBox("Minimum",1,3,4,COLOR_WHITE,COLOR_BLUE,&TextBoxFilterIsNumber, '|');;
+TextBox *maximum = new TextBox("Maximum",1,5,4,COLOR_WHITE,COLOR_BLUE,&TextBoxFilterIsNumber, '|');
+Screen game = Screen("Game",40,20,0,0);;
 
 bool tbFilter(int c) {
     return isdigit(c) || c == '*' || c == '+' || c == '-';
 }
 
+//extra quit method to free pointers
+void myQuit() {
+    for(auto &n : addresses) {;
+        delete n;
+    }
+    delete minimum;
+    delete maximum;
+    doQuit();
+}
+
 void doSolve() {
     for(int i = 0; i <= TO_SOLVE; i++) {
-        TextBox *tb = (TextBox*) game->getItemAt(i);
+        TextBox *tb = (TextBox*) game.getItemAt(i);
 
         int r = -9999999;
         if(tb->getValueLength() > 0)
@@ -31,24 +42,22 @@ void doSolve() {
         tb->setInteractable(false);
         tb->setColorFocus(COLOR_WHITE);
     }
-    game->getItemByName("Solve")->setVisible(false);
-    game->update();
+    game.getItemByName("Solve")->setVisible(false);
+    game.update();
 }
 
 void gameScreen() {
     if(minimum->getValueLength() == 0 || maximum->getValueLength() == 0) return;
-
     int minVal = std::stoi(minimum->getValue());
     int maxVal = std::stoi(maximum->getValue());
 
     if(minVal >= maxVal) return;
 
-    game = new Screen("Game",40,20,0,0);
-    Button *quit = new Button("Quit",1,19,COLOR_WHITE,COLOR_RED);
-    quit->onButtonPress = &doQuit;
-    Button *solve = new Button("Solve",1,16,COLOR_WHITE,COLOR_GREEN);
-    solve->onButtonPress = &doSolve;
-
+    Button quit = Button("Quit",1,19,COLOR_WHITE,COLOR_RED);
+    quit.onButtonPress = &myQuit;
+    Button solve = Button("Solve",1,16,COLOR_WHITE,COLOR_GREEN);
+    solve.onButtonPress = &doSolve;
+    
 
     std::random_device rd; // obtain a random number from hardware
     std::mt19937 gen(rd()); // seed the generator
@@ -81,43 +90,45 @@ void gameScreen() {
         textBoxText.append(std::to_string(num2));
         results.push_back(res);
         TextBox *tb = new TextBox(textBoxText,1,1+yOffet,7,COLOR_WHITE,COLOR_BLUE,&tbFilter, '=');
+        addresses.push_back(tb);
         yOffet += 2;
-        game->addItem(tb);
-        game->centerItem(game->X,tb);
+        game.addItem(tb);
+        game.centerItem(game.X,tb);
     }
-    game->addItem(solve);
-    game->addItem(quit);
-    game->centerItem(game->X,solve);
-    game->centerItem(game->X,quit);
-    game->draw();
+    
+    game.addItem(&solve);
+    game.addItem(&quit);
+    game.centerItem(game.X,&solve);
+    game.centerItem(game.X,&quit);
+    
+    game.draw();
 }
 
 
 int main(void) {
     
     initSclui();
-    Screen *menu = new Screen("Menu",40,14,0,0);
+    Screen menu = Screen("Menu",40,14,0,0);
 
-    Text *text = new Text("Welcome to SimplMath",1,1,COLOR_BLANK);
-    minimum = new TextBox("Minimum",1,3,4,COLOR_WHITE,COLOR_BLUE,&TextBoxFilterIsNumber, '|');
-    maximum = new TextBox("Maximum",1,5,4,COLOR_WHITE,COLOR_BLUE,&TextBoxFilterIsNumber, '|');
-    Button *play = new Button("Play",1,1,COLOR_WHITE,COLOR_GREEN);
-    play->onButtonPress = &gameScreen;
-    Button *quit = new Button("Quit",1,13,COLOR_WHITE,COLOR_RED);
-    quit->onButtonPress = &doQuit;
+    Text text = Text("Welcome to SimplMath",1,1,COLOR_BLANK);
+    
+    Button play = Button("Play",1,1,COLOR_WHITE,COLOR_GREEN);
+    play.onButtonPress = &gameScreen;
+    Button quit = Button("Quit",1,13,COLOR_WHITE,COLOR_RED);
+    quit.onButtonPress = &doQuit;
 
 
-    menu->addItem(text);
-    menu->addItem(minimum);
-    menu->addItem(maximum);
-    menu->addItem(play);
-    menu->addItem(quit);
-    menu->centerItem(menu->X,text);
-    menu->centerItem(menu->X,minimum);
-    menu->centerItem(menu->X,maximum);
-    menu->centerItem(menu->XY,play);
-    menu->centerItem(menu->X,quit);
-    menu->draw();
+    menu.addItem(&text);
+    menu.addItem(minimum);
+    menu.addItem(maximum);
+    menu.addItem(&play);
+    menu.addItem(&quit);
+    menu.centerItem(menu.X,&text);
+    menu.centerItem(menu.X,minimum);
+    menu.centerItem(menu.X,maximum);
+    menu.centerItem(menu.XY,&play);
+    menu.centerItem(menu.X,&quit);
+    menu.draw();
 
     return 0;
 }
